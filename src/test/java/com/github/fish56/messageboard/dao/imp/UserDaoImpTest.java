a49ee370b19c4ab32f0c0ec6ba8c964818733d78
apple <wow@github.com>
2019-03-15 11:15:02 +0800

    test

--- src/test/java/com/github/fish56/messageboard/dao/imp/UserDaoImpTest.java
+++ src/test/java/com/github/fish56/messageboard/dao/imp/UserDaoImpTest.java
@@ -4,48 +4,65 @@ import com.github.fish56.messageboard.entity.User;
 import com.github.fish56.messageboard.utils.Printer;
 import com.github.fish56.messageboard.utils.RandomString;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.List;
 
 public class UserDaoImpTest {
+    private static String nameOfNewCreatedUser;
+    private static String tokenOfNewCreatedUser;
+
+    // create a user before test, easy for
+    // follow test
+    @BeforeClass
+    public static void insertOne() {
+        User user = new User();
+        // create a new username with random string
+        // low probability that collision with exist username
+        String name = RandomString.Token.generateToken().substring(0,8);
+        user.setUsername(name);
+        user.setPassword("1232151");
+        String token = RandomString.Token.generateToken();
+        user.setToken(token);
+        boolean res = UserDaoImp.getInstance().insertOne(user);
+        Assert.assertTrue(res);
+
+        // save name and token for next test
+        nameOfNewCreatedUser = name;
+        tokenOfNewCreatedUser = token;
+    }
 
     @Test
     public void selectUserByToken() {
-        String token = "i86zPDI0TOocmXK4XAfv6toRj6AyU7";
+        String token = tokenOfNewCreatedUser;
         User res = UserDaoImp.getInstance().selectOneByToken(token);
-        Assert.assertEquals("Ache", res.getUsername());
+        Assert.assertEquals(nameOfNewCreatedUser, res.getUsername());
         Printer.user(res);
     }
 
-    @Test
-    public void insertOne() {
-        User user = new User();
-        user.setUsername("Ache");
-        user.setPassword("1232151");
-        user.setToken(RandomString.Token.generateToken());
-        boolean res = UserDaoImp.getInstance().insertOne(user);
-        Assert.assertTrue(res);
-    }
 
     @Test
     public void updateOne() {
         User user = new User();
-        user.setUsername("Ache");
-        user.setPassword("eee");
-        user.setToken(RandomString.Token.generateToken());
+        user.setUsername(nameOfNewCreatedUser);
+        user.setPassword("esdfeeesf");
+        String newToken = RandomString.Token.generateToken();
+        user.setToken(newToken);
         boolean res = UserDaoImp.getInstance().updateOne(user);
         Assert.assertTrue(res);
+
+        tokenOfNewCreatedUser = newToken;
     }
 
     @Test
     public void updateOne2() {
         User user = new User();
-        user.setUsername("NotExists");
+        user.setUsername("NotExistsName");
         user.setPassword("eee");
         user.setToken(RandomString.Token.generateToken());
         boolean res = UserDaoImp.getInstance().updateOne(user);
-        Assert.assertTrue(res);
+        Assert.assertFalse(res);
     }
 
     @Test
@@ -54,7 +71,7 @@ public class UserDaoImpTest {
 
     @Test
     public void selectOneByKey() {
-        User user = UserDaoImp.getInstance().selectOneByKey("ache");
+        User user = UserDaoImp.getInstance().selectOneByKey(nameOfNewCreatedUser);
         Assert.assertTrue(user != null);
         Printer.user(user);
     }
